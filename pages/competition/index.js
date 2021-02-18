5d8f25f1f61f8a84526a3018ff808e0d6e8dcb37
Tanguy Villiot <tanguy.villiot@ankward.fr>
2021-02-18 14:06:42 +0100

    Functionnal buttons

--- pages/competition/index.js
+++ pages/competition/index.js
@@ -1,14 +1,49 @@
 import {Button} from "react-bootstrap";
+import styles from '../../styles/competition.module.css';
+import {useState} from "react";
+
 
 export default function Competition(){
 
+    const[Component, setComponent] = useState("Submit");
+
+    let consoleLogActive = true;
+
+
+
+
+    const consoleLog = (message, active) =>{
+
+        if(active || consoleLogActive)
+        {
+            console.log(message);
+        }
+
+    }
+
+    const handleClick = (e) =>{
+
+        consoleLog(e.target.name);
+
+        setComponent(e.target.name);
+
+    }
+
+    const Container = () =>{
+
+        return <Component />
+    }
 
     return (
         <div>
             <div className="text-center">
-                <Button className="mr-2" variant="primary">Vote</Button>
-                <Button className="mr-2" variant="secondary" active>Submit</Button>
-                <Button className="mr-2" variant="success">Results</Button>
+                <Button name="Vote" className="mr-2" variant="primary" onClick={handleClick}>Vote</Button>
+                <Button name="Submit" className="mr-2" variant="secondary" active onClick={handleClick}>Submit</Button>
+                <Button name="Results" className="mr-2" variant="success" onClick={handleClick}>Results</Button>
+            </div>
+            <div className={styles.container}>
+                <Container />
+
             </div>
         </div>
 
