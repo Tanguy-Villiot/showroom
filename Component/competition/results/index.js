558f6abd575df4d7b28ad4ffe60ac8265b7c95ab
Tanguy Villiot <tanguy.villiot@ankward.fr>
2021-02-21 16:07:12 +0100

    add default dataTable in result page

--- Component/competition/results/index.js
+++ Component/competition/results/index.js
@@ -1,7 +1,50 @@
+import { MDBDataTable } from 'mdbreact';
+
+
 export default function Results(){
+
+    const dataValue = {
+        columns: [
+            {
+                label: 'Image',
+                field: 'name',
+                sort: 'asc',
+                width: 150
+            },
+            {
+                label: 'Utilisateur',
+                field: 'email',
+                sort: 'asc',
+                width: 270
+            },
+            {
+                label: 'Votes',
+                field: 'votes',
+                sort: 'asc',
+                width: 200
+            },
+            {
+                label: 'positions',
+                field: 'pos',
+                sort: 'asc',
+                width: 100
+            }
+        ],
+        rows: [
+
+        ]
+    };
+
 
     return <div>
         <h1>Results</h1>
+
+        <MDBDataTable
+            striped
+            bordered
+            small
+            data={dataValue}
+        />
     </div>
 
 }
