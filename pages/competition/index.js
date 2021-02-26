f895a172c32543559b637188acf3b251c5f70719
Tanguy Villiot <tanguy.villiot@ankward.fr>
2021-02-26 11:42:13 +0100

    Remove ancient page

--- pages/competition/index.js
+++ pages/competition/index.js
@@ -30,41 +30,12 @@ export default function Competition(){
 
     }
 
-    const Container = () =>{
-
-        let Comp = Submit;
-
-        switch (Component)
-        {
-            case "Submit":
-                Comp = Submit;
-                break;
-            case "Vote":
-                Comp = Vote;
-                break;
-            case "Results":
-                Comp = Results;
-                break;
-            default:
-                Comp = Submit;
-                break;
-
-        }
-
-        return <Comp />;
-
-    }
 
 
     return (
         <div>
-            <div className="text-center">
-                <Button name="Vote" className="mr-2" variant="primary" onClick={handleClick}>Vote</Button>
-                <Button name="Submit" className="mr-2" variant="secondary" active onClick={handleClick}>Submit</Button>
-                <Button name="Results" className="mr-2" variant="success" onClick={handleClick}>Results</Button>
-            </div>
             <div className="container-md mt-5">
-                <Container/>
+                
 
             </div>
         </div>
