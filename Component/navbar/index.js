1ff9d4419e90eb15d17a1c16773baceabf5ff40d
Tanguy Villiot <tanguy.villiot@ankward.fr>
2021-02-28 00:00:19 +0100

    Add modal connection and button in navbar

--- Component/navbar/index.js
+++ Component/navbar/index.js
@@ -1,7 +1,16 @@
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Button, Modal, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import Link from 'next/link'
+import {useState} from "react";
+import {MDBBtn} from "mdbreact";
 
 export default function NavBar(){
+
+    const [modalShow, setModalShow] = useState(false);
+
+    //Effect method
+    const handleClickConnection = () =>{
+        setModalShow(true);
+    }
 
 
     return(
@@ -23,8 +32,35 @@ export default function NavBar(){
                     <Link href="/register">
                         <Nav.Link href="#home">Register</Nav.Link>
                     </Link>
+                    /
+                    <Nav.Link onClick={handleClickConnection}>Connection</Nav.Link>
                 </Navbar.Collapse>
             </Navbar>
+
+            {/*Sign in modal*/}
+            <Modal show={modalShow} onHide={() => setModalShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Sign in
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                            Your email
+                        </label>
+                        <input type="email" id="defaultFormLoginEmailEx" className="form-control" />
+                        <br />
+                        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                            Your password
+                        </label>
+                        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
+                        <div className="text-center mt-4">
+                            <MDBBtn color="primary" type="submit">Login</MDBBtn>
+                        </div>
+                    </form>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 
