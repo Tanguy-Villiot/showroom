19e6ce6709a53777027db2704ec4782082212a8d
Tanguy Villiot <tanguy.villiot@ankward.fr>
2021-02-28 20:46:08 +0100

    Add authentification system: sign-in, sign-out, and a private page

---
+++ pages/private/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { withIronSession } from "next-iron-session";
+
+export default function PrivatePage({ user }) {
+
+    return(
+        <div>
+            <h1>Hello {user.email}</h1>
+            <p>Secret things live here...</p>
+        </div>
+
+    )
+}
+
+
+export const getServerSideProps = withIronSession(
+    async ({ req, res }) => {
+        const user = req.session.get("user");
+
+        if (!user) {
+            res.statusCode = 404;
+            res.end();
+            return { props: {} };
+        }
+
+        return {
+            props: { user }
+        };
+    },
+    {
+        cookieName: "MYSITECOOKIE",
+        cookieOptions: {
+            secure: process.env.NODE_ENV === "production" ? true : false
+        },
+        password: process.env.APPLICATION_SECRET
+    }
+);
+
