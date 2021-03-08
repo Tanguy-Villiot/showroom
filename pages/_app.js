5dc3d46b33c2f0e83c5f0f963bd8da93e581cd8d
Tanguy Villiot <tanguy.villiot@ankward.fr>
2021-03-09 00:03:43 +0100

    Page transition add

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,59 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //Component Import
 import NavBar from "../Component/navbar";
 import Toastify, {ToastifyContext} from "../Component/toastify";
+import { PageTransition } from 'next-page-transitions'
+import Loader from "../Component/Loader";
 
+
+
+const TIMEOUT = 400
 
 function MyApp({ Component, pageProps }) {
     return <div className="App">
         <ToastifyContext.Provider value={new Toastify()}>
             <NavBar/>
-            <Component {...pageProps} />
+
+            <PageTransition
+                timeout={TIMEOUT}
+                classNames="page-transition"
+                loadingComponent={<Loader />}
+                loadingDelay={500}
+                loadingTimeout={{
+                    enter: TIMEOUT,
+                    exit: 0,
+                }}
+                loadingClassNames="loading-indicator"
+            >
+                <Component {...pageProps} />
+            </PageTransition>
+            <style jsx global>{`
+        .page-transition-enter {
+          opacity: 0;
+          transform: translate3d(0, 20px, 0);
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transition: opacity ${TIMEOUT}ms;
+        }
+        .loading-indicator-appear,
+        .loading-indicator-enter {
+          opacity: 0;
+        }
+        .loading-indicator-appear-active,
+        .loading-indicator-enter-active {
+          opacity: 1;
+          transition: opacity ${TIMEOUT}ms;
+        }
+      `}</style>
+
         </ToastifyContext.Provider>
 
     </div>
